534efb39c25c4e9f04a0fac0e34d5d63c0d92094
Vladimír Mrkáček <vlad.mrkacek@gmail.com>
2025-02-10 23:08:58 +0100

    feat(vue): prepared guard clause and refactor
    testing longer commit message
    
    BREAKING CHANGE: there is actually no breaking change

--- src/example-vue-project/src/router.ts
+++ src/example-vue-project/src/router.ts
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router"
-import OrdersList from './components/OrdersList.vue'
+import OrdersListPage from './pages/orders/OrdersListPage.vue'
 import LoginPage from './pages/auth/LoginPage.vue'
 
 const routes = [
-  { path: '/orders', component: OrdersList },
+  { path: '/orders', component: OrdersListPage },
   { path: '/login', component: LoginPage },
 ]
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+//TODO: Add auth guard
+router.beforeEach((to, from) => {
+  console.log(to, from);
 })
